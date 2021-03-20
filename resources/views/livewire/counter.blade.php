bbbe842cdac0181119a22acca3a0c7f5fd057407
alexchen <alexchen@tycets101deMacBook-Pro.local>
2021-03-20 12:42:50 +0800

    create livewire counter

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,9 @@
+<div>
+    {{-- Care about people's approval and you will be their prisoner. --}}
+    <h1>HELLO WORLD!</h1>
+
+    <div style="text-align: center">
+        <button wire:click="increment">+</button>
+        <h2>{{ $count }}</h2>
+    </div>
+</div>
